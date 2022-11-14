7d71ee31e96fc1b3918723163c7bd5e9c48ce3fa
rcpang <rcpang@UNC-R90YDFW2.localdomain>
2022-11-14 15:36:49 -0500

    second submission

--- bin/cli.js
+++ bin/cli.js
@@ -7,7 +7,6 @@ const args = minimist(process.argv.slice(2));
 const sides = args.sides || 6;
 const dice = args.dice || 2;
 const rolls = args.rolls || 1;
-const results = args.results;
 
-const theOutput = JSON.stringify(roll(sides, dice, rolls, results));
+const theOutput = JSON.stringify(roll(sides, dice, rolls));
 console.log(theOutput);
