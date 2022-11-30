50bfaba9c5142768a0bc8b59da3d6c8d31fd566f
rcpang <rcpang@UNC-R90YDFW2.localdomain>
2022-11-30 16:34:50 -0500

    changes to package and cli

--- bin/cli.js
+++ bin/cli.js
@@ -6,6 +6,6 @@ import minimist from "minimist";
 const args = minimist(process.argv.slice(2));
 var sides = args.sides || 6;
 var dice = args.dice || 2;
-var rolls = args.roll || 1;
+var rolls = args.rolls || 1;
 
 console.log(JSON.stringify(roll(sides, dice, rolls));
