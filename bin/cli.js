f9d023671f22678da1be03c3000e5f0f10aba6b0
rcpang <rcpang@UNC-R90YDFW2.localdomain>
2022-12-04 17:16:44 -0500

    more changes

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,10 @@
 #!usr/bin/env node
 
 import { roll } from "../lib/roll.js";
-import minimist from "minimist";
+import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
-var sides = args.sides || 6;
-var dice = args.dice || 2;
-var rolls = args.rolls || 1;
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
 console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls));
