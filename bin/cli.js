715127cfa400a3f698cad9fff598bad32fcf6a2f
rcpang <rcpang@UNC-R90YDFW2.localdomain>
2022-12-04 17:04:29 -0500

    fixed some things

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,4 @@
-#!usr/bin/env mode
+#!usr/bin/env node
 
 import { roll } from "../lib/roll.js";
 import minimist from "minimist";
@@ -7,5 +7,4 @@ const args = minimist(process.argv.slice(2));
 var sides = args.sides || 6;
 var dice = args.dice || 2;
 var rolls = args.rolls || 1;
-
-console.log(JSON.stringify(roll(sides, dice, rolls));
+console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls));
