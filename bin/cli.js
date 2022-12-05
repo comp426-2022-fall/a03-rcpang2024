dffa30f7ea712855f35cf309a6b3f7e88af4824e
rcpang <rcpang@UNC-R90YDFW2.localdomain>
2022-12-04 22:08:46 -0500

    condition checking

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,8 @@ import { roll } from "../lib/roll.js";
 import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
-const sides = args.sides || 6;
-const dice = args.dice || 2;
-const rolls = args.rolls || 1;
+const sides = args.sides && args.sides > 0 ? args.sides: 6;
+const dice = args.dice && args.dice > 0 ? args.dice: 2;
+const rolls = args.rolls && args.rolls > 0 ? args.rolls: 1;
 console.log(JSON.stringify(roll(sides, dice, rolls)));
+process.exit(0);
